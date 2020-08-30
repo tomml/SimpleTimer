71f09396a4e24d53b2fa383d6f6f5f76e207bc5f
Thomas Schauer <ts.schauer@gmail.com>
2020-08-30 18:51:28 +0200

    Initial Commit

---
+++ SimpleTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTimer
+//
+//  Created by Thomas Schauer on 30.08.20.
+//  Copyright © 2020 Thomas Schauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
